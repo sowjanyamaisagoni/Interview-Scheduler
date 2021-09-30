cb04f0fbf014df4f7aa412045388f64c88bc3ef2
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-09-30 17:24:09 -0400

    The Schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
 //import React from "react";
 
 import "components/Application.scss";
-
+import Appointment from "components/Appointment";
 import React, { useState } from "react";
 
 import DayList from "components/DayList";
@@ -24,10 +24,58 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Arche Cohen",
+      interviewer: {
+        id: 3,
+        name: "Tori Molcolm",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "MAria Boucher",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
+    },
+  },
+];
+
+
 export default function Application() {
 
   const [day, setDay] = useState("Monday");
-
+  console.log("Application.js > day:", day);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -50,6 +98,10 @@ export default function Application() {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
+        <Appointment key={"last"} time={"5PM"} />
       </section>
     </main>
   );
