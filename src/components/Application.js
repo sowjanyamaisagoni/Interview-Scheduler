2ca20674ec202e0c768701646a74f7c953adbd8f
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-10-05 10:03:40 -0400

    Separation of Concerns

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,23 +1,20 @@
-//import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DayList from 'components/DayList';
+import Appointment from 'components/Appointment';
+import {
+	getAppointmentsForDay,
+	getInterview,
+	getInterviewersForDay,
+} from 'helpers/selectors';
+import useApplicationData from 'hooks/useApplicationData';
 
-import "components/Application.scss";
-import Appointment from "components/Appointment";
-import React, { useState } from "react";
-import axios from "axios";
-import { useEffect } from "react"; 
-import DayList from "components/DayList";
-import { getAppointmentsForDay, getInterview ,getInterviewersForDay} from "helpers/selectors";
+import 'components/Application.scss';
 
-export default function Application() {
+function Application(props) {
+  const { state, setState, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {},
-  });
-
-  const setDay = (day) => setState({...state,day});
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
@@ -32,80 +29,25 @@ export default function Application() {
       }));
     });
   }, []);
+  const interviewers = getInterviewersForDay(state, state.day);
 
-  console.log("Application.js > today:", state.day);
-  
-    const appointments = getAppointmentsForDay(state, state.day).map((appointment) => {
-      const interview = getInterview(state, appointment.interview);
-
+  const appointments = getAppointmentsForDay(state, state.day).map(
+    (appointment) => {
       return (
         <Appointment
-          {...appointment}
           key={appointment.id}
+          {...appointment}
           interview={getInterview(state, appointment.interview)}
-          interviewers={getInterviewersForDay(state, state.day)}
+          interviewers={interviewers}
           bookInterview={bookInterview}
           cancelInterview={cancelInterview}
         />
       );
-    });
-  
-  function bookInterview(id, interview) {
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    return axios
-      .put(`/api/appointments/${id}`, {
-        ...state.appointments[id],
-        interview,
-      })
-      .then((res) => {
-        setState((prev) => ({
-          ...prev,
-          appointments,
-        }));
-      }); 
-  }
-  
-  function cancelInterview(id) {
-    // console.log('cancelInterview id:', id);
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    return axios
-      .delete(`/api/appointments/${id}`, {
-        ...state.appointments[id],
-        interview: null,
-      })
-      .then((res) => {
-        // console.log('axios.put res: \n', res);
-        setState((prev) => ({
-          ...prev,
-          appointments,
-        }));
-      });
-  }
-
+    }
+  );
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -115,7 +57,6 @@ export default function Application() {
         <nav className="sidebar__menu">
           <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
-
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -123,13 +64,10 @@ export default function Application() {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
         {appointments}
-        {/* {appointments.map((appointment) => (
-          <Appointment key={appointment.id} {...appointment} />
-        ))} */}
         <Appointment key={"last"} time={"5PM"} />
       </section>
     </main>
   );
 }
+export default Application;
