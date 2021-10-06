cc17ffcfea036989f480f5fd7baf0a30b2c28266
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-10-06 10:21:55 -0400

    Coverage Overlap

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -43,7 +43,7 @@ it("validates that the student name is not blank", () => {
   expect(onSave).not.toHaveBeenCalled();
 });
 
-it("calls onSave function when the name is defined", () => {
+xit("calls onSave function when the name is defined", () => {
   /* 1. Create the mock onSave function */
   const onSave = jest.fn();
 
@@ -63,4 +63,18 @@ it("calls onSave function when the name is defined", () => {
   expect(onSave).toHaveBeenCalledTimes(1);
   expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
 });
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+
+    const input = getByPlaceholderText("Enter Student Name");
+
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
 });
